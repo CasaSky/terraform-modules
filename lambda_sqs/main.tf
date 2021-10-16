029ba4763f15419304535a5b0d4fcda43ec5fe88
Talal Tabia <mail@talaltabia.com>
2021-10-16 20:31:02 +0200

    rename mapping

--- lambda_sqs/main.tf
+++ lambda_sqs/main.tf
@@ -11,7 +11,7 @@ module "sqs" {
   name = var.function_name
 }
 
-resource "aws_lambda_event_source_mapping" "example" {
+resource "aws_lambda_event_source_mapping" "sqs_mapping" {
   event_source_arn = module.sqs.arn
   function_name    = module.lambda.arn
 }
