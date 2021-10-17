031b002d2c800a455109c5ec2a8697c93630a97f
Talal Tabia <mail@talaltabia.com>
2021-10-17 10:28:57 +0200

    add dlq

--- sqs/variables.tf
+++ sqs/variables.tf
@@ -6,3 +6,8 @@ variable "delay_seconds" {
   description = "The time in seconds that the delivery of all messages in the queue will be delayed"
   default = 1
 }
+
+variable "redrive_policy" {
+  description = "The JSON policy to set up the Dead Letter Queue"
+  default = ""
+}
