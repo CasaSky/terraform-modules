94202271d6187097e2be70c585c931351a1d6e67
Talal Tabia <mail@talaltabia.com>
2021-10-17 11:48:36 +0200

    fix delegating env variables

--- lambda-sqs/main.tf
+++ lambda-sqs/main.tf
@@ -7,9 +7,7 @@ module "lambda" {
   runtime       = var.runtime
   memory_size   = var.memory_size
 
-  environment = {
-    variables = var.environment_variables
-  }
+  environment_variables = var.environment_variables
 
   tags = var.tags
 }
