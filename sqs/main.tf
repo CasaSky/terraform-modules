a16cbf86ad0fbc817e06aef453c3e47b26f2b0b8
Talal Tabia <mail@talaltabia.com>
2021-10-17 17:34:31 +0200

    delegate deadletter queue policy

--- sqs/main.tf
+++ sqs/main.tf
@@ -2,6 +2,7 @@ resource "aws_sqs_queue" "this" {
   name                      = var.name
   delay_seconds             = var.delay_seconds
   message_retention_seconds = var.message_retention_seconds
+  redrive_policy            = var.redrive_policy
   kms_master_key_id         = var.kms_master_key_id
 
   tags = var.tags
