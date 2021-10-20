ec24c497d82c37e0912858958ef3d1ce5cad98c2
Talal Tabia <mail@talaltabia.com>
2021-10-20 12:25:40 +0200

    specify a provider configuration for the module

--- lambda-sqs-sns/versions.tf
+++ lambda-sqs-sns/versions.tf
@@ -1,8 +1,9 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.63"
+      source                = "hashicorp/aws"
+      version               = "~> 3.63"
+      configuration_aliases = [aws.alternate]
     }
   }
 
